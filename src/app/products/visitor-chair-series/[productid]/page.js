2816ee505f50af7a0ae28216e064715f4c9967bb
sachin4369 <sachinchochi@gmail.com>
2023-10-24 12:22:22 +0530

    try catch added

--- src/app/products/visitor-chair-series/[productid]/page.js
+++ src/app/products/visitor-chair-series/[productid]/page.js
@@ -4,138 +4,148 @@ import Image from "next/image";
 import SocialShareIcon from "@/app/components/SocialShare/page";
 
 const getVisitorChairData = async (id) => {
-  const apiUrl = process.env.NEXT_PUBLIC_API_URL;
-    let data = await fetch(`${apiUrl}/api/visitorapi/${id}`);
-  data = await data.json();
-  return data.result;
+  try {
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+      let data = await fetch(`${apiUrl}/api/visitorapi/${id}`);
+    data = await data.json();
+    return data.result;
+  } catch (error) {
+    console.error("not found", error)
+    return null;
+  }
 };
 
 const Page = async ({ params }) => {
-  const visitor = await getVisitorChairData(params.productid);
-  return (
-    <>
-      <nav className="flex mt-5 mb-8" aria-label="Breadcrumb">
-        <ol className="inline-flex items-center space-x-1 md:space-x-3">
-          <li className="inline-flex items-center">
-            <Link
-              href="/"
-              className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white"
-            >
-              <svg
-                className="w-3 h-3 mr-2.5"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-              >
-                <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
-              </svg>
-              Home
-            </Link>
-          </li>
-          <li>
-            <div className="flex items-center">
-              <svg
-                className="w-3 h-3 text-gray-400 mx-1"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 6 10"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="m1 9 4-4-4-4"
-                />
-              </svg>
+  try {
+    const visitor = await getVisitorChairData(params.productid);
+    return (
+      <>
+        <nav className="flex mt-5 mb-8" aria-label="Breadcrumb">
+          <ol className="inline-flex items-center space-x-1 md:space-x-3">
+            <li className="inline-flex items-center">
               <Link
-                href="/products/visitor-chair-series"
-                className="ml-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ml-2 dark:text-gray-400 dark:hover:text-white"
+                href="/"
+                className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white"
               >
-                Visitor Chair Series
+                <svg
+                  className="w-3 h-3 mr-2.5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
+                </svg>
+                Home
               </Link>
-            </div>
-          </li>
-          <li aria-current="page">
-            <div className="flex items-center">
-              <svg
-                className="w-3 h-3 text-gray-400 mx-1"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 6 10"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="m1 9 4-4-4-4"
-                />
-              </svg>
-              <span className="ml-1 text-sm font-medium text-gray-500 md:ml-2 dark:text-gray-400">
-                {visitor.Name}
-              </span>
-            </div>
-          </li>
-        </ol>
-      </nav>
-      <div className="flex flex-col items-center bg-white border pl-0 md:pl-20 border-gray-200 rounded-lg shadow md:flex-row dark:border-gray-700 dark:bg-gray-800 p-4">
-      <div className="w-72">
-        <Image
-            className="object-cover"
-            width={400}
-            height={400}
-            src={visitor.Url}
-            alt={visitor.Name}
-          />
-        </div>
-        <div className="flex flex-col justify-between leading-normal pl-0 md:pl-20">
-          <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            {visitor.Name}
-          </h2>
-          <div className="font-normal text-sm text-gray-700 space-y-2">
-            <p>Model No. : {visitor.ModelNo}</p>
-            <p>Type : {visitor.Type}</p>
-            <p>Fabric : {visitor.Fabric}</p>
-            <p>Mechanism : {visitor.Mechanism}</p>
-            <p>Armrest : {visitor.Armrest}</p>
-            <p>Mechanism : {visitor.Mechanism}</p>
-            <p>BodyColor : {visitor.BodyColor}</p>
-            <p>Base Material : {visitor.BaseMaterial}</p>
-            <p>Height Adjuster : {visitor.HeightAdjuster}</p>
-            <p className="flex space-x-2 items-center">
-              Available Colors :{" "}
-              <span
-                title="Black"
-                className="w-4 h-4 bg-gray-700 rounded ml-1"
-              ></span>
-              <span title="Blue" className="w-4 h-4 bg-blue-600 rounded"></span>
-              <span
-                title="Green"
-                className="w-4 h-4 bg-green-500 rounded"
-              ></span>
-              <span title="red" className="w-4 h-4 bg-red-500 rounded"></span>
-              <span
-                title="Orange"
-                className="w-4 h-4 bg-orange-500 rounded"
-              ></span>
-              <span
-                title="Yellow"
-                className="w-4 h-4 bg-yellow-300 rounded"
-              ></span>
-            </p>
-            <div className="flex items-center">
-              Share on : &nbsp;
-              <SocialShareIcon />
+            </li>
+            <li>
+              <div className="flex items-center">
+                <svg
+                  className="w-3 h-3 text-gray-400 mx-1"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 6 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m1 9 4-4-4-4"
+                  />
+                </svg>
+                <Link
+                  href="/products/visitor-chair-series"
+                  className="ml-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ml-2 dark:text-gray-400 dark:hover:text-white"
+                >
+                  Visitor Chair Series
+                </Link>
+              </div>
+            </li>
+            <li aria-current="page">
+              <div className="flex items-center">
+                <svg
+                  className="w-3 h-3 text-gray-400 mx-1"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 6 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m1 9 4-4-4-4"
+                  />
+                </svg>
+                <span className="ml-1 text-sm font-medium text-gray-500 md:ml-2 dark:text-gray-400">
+                  {visitor.Name}
+                </span>
+              </div>
+            </li>
+          </ol>
+        </nav>
+        <div className="flex flex-col items-center bg-white border pl-0 md:pl-20 border-gray-200 rounded-lg shadow md:flex-row dark:border-gray-700 dark:bg-gray-800 p-4">
+        <div className="w-72">
+          <Image
+              className="object-cover"
+              width={400}
+              height={400}
+              src={visitor.Url}
+              alt={visitor.Name}
+            />
+          </div>
+          <div className="flex flex-col justify-between leading-normal pl-0 md:pl-20">
+            <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              {visitor.Name}
+            </h2>
+            <div className="font-normal text-sm text-gray-700 space-y-2">
+              <p>Model No. : {visitor.ModelNo}</p>
+              <p>Type : {visitor.Type}</p>
+              <p>Fabric : {visitor.Fabric}</p>
+              <p>Mechanism : {visitor.Mechanism}</p>
+              <p>Armrest : {visitor.Armrest}</p>
+              <p>Mechanism : {visitor.Mechanism}</p>
+              <p>BodyColor : {visitor.BodyColor}</p>
+              <p>Base Material : {visitor.BaseMaterial}</p>
+              <p>Height Adjuster : {visitor.HeightAdjuster}</p>
+              <p className="flex space-x-2 items-center">
+                Available Colors :{" "}
+                <span
+                  title="Black"
+                  className="w-4 h-4 bg-gray-700 rounded ml-1"
+                ></span>
+                <span title="Blue" className="w-4 h-4 bg-blue-600 rounded"></span>
+                <span
+                  title="Green"
+                  className="w-4 h-4 bg-green-500 rounded"
+                ></span>
+                <span title="red" className="w-4 h-4 bg-red-500 rounded"></span>
+                <span
+                  title="Orange"
+                  className="w-4 h-4 bg-orange-500 rounded"
+                ></span>
+                <span
+                  title="Yellow"
+                  className="w-4 h-4 bg-yellow-300 rounded"
+                ></span>
+              </p>
+              <div className="flex items-center">
+                Share on : &nbsp;
+                <SocialShareIcon />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  } catch (error) {
+    console.error("An error occurred:", error);
+      return (<div>An error occurred while loading data.</div>);
+  }
 };
 
 export default Page;
--- src/app/products/workstation-chair-series/page.js
+++ src/app/products/workstation-chair-series/page.js
@@ -2,129 +2,139 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 const getWorkStationChairData = async () =>{
-  const apiUrl = process.env.NEXT_PUBLIC_API_URL;
-  let data = await fetch(`${apiUrl}/api/workstationapi/`)
-    data = await data.json();
-    return data;
+  try {
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+    let data = await fetch(`${apiUrl}/api/workstationapi/`)
+      data = await data.json();
+      return data;
+  } catch (error) {
+    console.error("not found", error)
+    return null;
+  }
 }
 
 const WorkStationChairSeries = async () => {
-    const workStationChair = await getWorkStationChairData();
-  return (
-    <>
-        <nav className="flex mt-5" aria-label="Breadcrumb">
-        <ol className="inline-flex items-center space-x-1 md:space-x-3">
-          <li className="inline-flex items-center">
-            <Link
-              href="/"
-              className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white"
-            >
-              <svg
-                className="w-3 h-3 mr-2.5"
-                aria-hidden="true"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-              >
-                <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
-              </svg>
-              Home
-            </Link>
-          </li>
-          <li>
-            <div className="flex items-center">
-              <svg
-                className="w-3 h-3 text-gray-400 mx-1"
-                aria-hidden="true"
-                fill="none"
-                viewBox="0 0 6 10"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="m1 9 4-4-4-4"
-                />
-              </svg>
+    try {
+      const workStationChair = await getWorkStationChairData();
+    return (
+      <>
+          <nav className="flex mt-5" aria-label="Breadcrumb">
+          <ol className="inline-flex items-center space-x-1 md:space-x-3">
+            <li className="inline-flex items-center">
               <Link
-                href="/products"
-                className="ml-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ml-2 dark:text-gray-400 dark:hover:text-white"
+                href="/"
+                className="inline-flex items-center  text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white"
               >
-                Products
+                <svg
+                  className="w-3 h-3 mr-2.5"
+                  aria-hidden="true"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
+                </svg>
+                Home
+              </Link>
+            </li>
+            <li>
+              <div className="flex items-center">
+                <svg
+                  className="w-3 h-3 text-gray-400 mx-1"
+                  aria-hidden="true"
+                  fill="none"
+                  viewBox="0 0 6 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m1 9 4-4-4-4"
+                  />
+                </svg>
+                <Link
+                  href="/products"
+                  className="ml-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ml-2 dark:text-gray-400 dark:hover:text-white"
+                >
+                  Products
+                </Link>
+              </div>
+            </li>
+            <li aria-current="page">
+              <div className="flex items-center">
+                <svg
+                  className="w-3 h-3 text-gray-400 mx-1"
+                  aria-hidden="true"
+                  fill="none"
+                  viewBox="0 0 6 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m1 9 4-4-4-4"
+                  />
+                </svg>
+                <span className="ml-1 text-sm font-medium text-gray-500 md:ml-2 dark:text-gray-400">
+                  Work Station Chairs
+                </span>
+              </div>
+            </li>
+          </ol>
+        </nav>
+  
+        <h1 className="text-3xl font-semibold my-5">Work Station Chairs</h1>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          {workStationChair.map((item) => (
+            <div
+              key={item.id}
+              className="bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 p-5"
+            >
+              <Image
+                className="rounded-t-lg h-40 mx-auto object-fit" 
+                width={120}
+                height={100}
+                src={item.Url} 
+                alt={item.Name} />
+              <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                {item.Name}
+              </h2>
+              <p className="font-normal text-gray-700 space-y-1 flex flex-col mb-4 text-sm">
+                <span>{item.ModelNo}</span>
+                <span>{item.Type}</span>
+                <span>{item.Armrest}</span>
+                <span>{item.Fabric}</span>
+              </p>
+              <Link
+                href={`/products/workstation-chair-series/${item.id}`}
+                className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Click here for more details
+                <svg
+                  className="w-3.5 h-3.5 ml-2"
+                  aria-hidden="true"
+                  fill="none"
+                  viewBox="0 0 14 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M1 5h12m0 0L9 1m4 4L9 9"
+                  />
+                </svg>
               </Link>
             </div>
-          </li>
-          <li aria-current="page">
-            <div className="flex items-center">
-              <svg
-                className="w-3 h-3 text-gray-400 mx-1"
-                aria-hidden="true"
-                fill="none"
-                viewBox="0 0 6 10"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="m1 9 4-4-4-4"
-                />
-              </svg>
-              <span className="ml-1 text-sm font-medium text-gray-500 md:ml-2 dark:text-gray-400">
-                Work Station Chairs
-              </span>
-            </div>
-          </li>
-        </ol>
-      </nav>
-
-      <h1 className="text-3xl font-semibold my-5">Work Station Chairs</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {workStationChair.map((item) => (
-          <div
-            key={item.id}
-            className="bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 p-5"
-          >
-            <Image
-              className="rounded-t-lg h-40 mx-auto object-fit" 
-              width={120}
-              height={100}
-              src={item.Url} 
-              alt={item.Name} />
-            <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              {item.Name}
-            </h2>
-            <p className="font-normal text-gray-700 space-y-1 flex flex-col mb-4 text-sm">
-              <span>{item.ModelNo}</span>
-              <span>{item.Type}</span>
-              <span>{item.Armrest}</span>
-              <span>{item.Fabric}</span>
-            </p>
-            <Link
-              href={`/products/workstation-chair-series/${item.id}`}
-              className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Click here for more details
-              <svg
-                className="w-3.5 h-3.5 ml-2"
-                aria-hidden="true"
-                fill="none"
-                viewBox="0 0 14 10"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M1 5h12m0 0L9 1m4 4L9 9"
-                />
-              </svg>
-            </Link>
-          </div>
-        ))}
-      </div>
-    </>
-  )
+          ))}
+        </div>
+      </>
+    )
+    } catch (error) {
+      console.error("An error occurred:", error);
+      return (<div>An error occurred while loading data.</div>);
+    }
 }
 
 export default WorkStationChairSeries;
